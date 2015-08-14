159d6a55becb4f507c9a4a49b15f5ca14d8be6a2
Chris Holdgraf <choldgraf@berkeley.edu>
2015-08-14 09:18:54 -0700

    Added a diagonal axis tool

--- viz.py
+++ viz.py
@@ -1,8 +1,11 @@
 """Convenience functions for plotting."""
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Affine2D
+import mpl_toolkits.axisartist.floating_axes as floating_axes
 
-__all__ = ['split_color_axis']
+__all__ = ['split_color_axis', 'add_rotated_axis']
 
 
 def split_color_axis(ax, cutoff=0, cols=['g', 'r']):
@@ -27,3 +30,50 @@ def split_color_axis(ax, cutoff=0, cols=['g', 'r']):
         ax.collections[0].set_color(np.where(over, *cols))
     else:
         raise ValueError('Axis must have a scatterplot or histogram')
+
+
+def add_rotated_axis(f, extents=(-1, 1, 0, 1), sc_x=None, sc_y=None,
+                     rotation=-45, position=(.5, .5), invisible_border=True):
+    """Add a rotated axis to an existing figure.
+
+    Parameters
+    ----------
+    f : mpl.figure
+        The figure you're adding a axis to.
+    extents : tuple, shape of ints (4,)
+        The x min/max and y min/max of the axis
+    sc_x : float
+        How much to scale the x-axis for shaping nicely
+    sc_y : float
+        How much to scale the y_axis for shaping nicely
+    rotation : float
+        Rotation of the axis
+    position : tuple of floats, shape (2,)
+        The position of the axis as a fraction of the figure
+    invisible_border : bool
+        Whether to make elements of the axis invisible
+
+    Returns
+    -------
+    ax : mpl axis object
+        The axis you've added
+    ax_aux : mpl auxilliary axis object
+        The auxilliary object of the axis you've added. This
+        is useful for doing certain kinds of transformations that
+        aren't possible with the regular axis.
+    """
+    af = Affine2D()
+    transform = af.scale(sc_x, sc_y).rotate_deg(-45)
+    helper = floating_axes.GridHelperCurveLinear(transform, extents)
+    ax = floating_axes.FloatingSubplot(f, 111, grid_helper=helper)
+    ax_aux = ax.get_aux_axes(transform)
+    f.add_subplot(ax)
+    ax.set_position(np.hstack([position, .5, .5]))
+
+    if invisible_border is True:
+        # Strip axis elements
+        ax.patch.set(visible=False)
+        for axis in ax.axis.values():
+            axis.set_visible(False)
+
+    return ax, ax_aux
