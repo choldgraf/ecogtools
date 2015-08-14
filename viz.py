Fixed the rotation problems

viz.py
@@ -33,7 +33,8 @@ def split_color_axis(ax, cutoff=0, cols=['g', 'r']):
 
 
 def add_rotated_axis(f, extents=(-1, 1, 0, 1), sc_x=None, sc_y=None,
-                     rotation=45, position=(.5, .5), invisible_border=True):
+                     rotation=45, position=(.5, .5, .1, .1),
+                     invisible_border=True):
     """Add a rotated axis to an existing figure.
 
     Parameters
@@ -41,7 +42,7 @@ def add_rotated_axis(f, extents=(-1, 1, 0, 1), sc_x=None, sc_y=None,
     f : mpl.figure
         The figure you're adding a axis to.
     extents : tuple, shape of ints (4,)
-        The x min/max and y min/max of the axis
+        The x min/max and y min/max of the axis, as well as axis data boundaries.
     sc_x : float
         How much to scale the x-axis for shaping nicely
     sc_y : float
@@ -67,9 +68,9 @@ def add_rotated_axis(f, extents=(-1, 1, 0, 1), sc_x=None, sc_y=None,
     helper = floating_axes.GridHelperCurveLinear(transform, extents)
     ax = floating_axes.FloatingSubplot(f, 111, grid_helper=helper)
     ax_aux = ax.get_aux_axes(transform)
-    ax.invert_xaxis()
     f.add_subplot(ax)
-    ax.set_position(np.hstack([position, .5, .5]))
+    ax.set_position(position)
+    ax.invert_xaxis()
 
     if invisible_border is True:
         # Strip axis elements