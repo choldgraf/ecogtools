22ad7b89d3e9820de2771693b0e452d24352e1c4
Chris Holdgraf <choldgraf@berkeley.edu>
2015-11-10 10:43:02 -0800

    Added some functions to do label shuffling until the new sklearn implements this automatically

--- cross_validation.py
+++ cross_validation.py
@@ -2,116 +2,56 @@
 
 import numpy as np
 from sklearn import cross_validation as cval
+from sklearn.cross_validation import PredefinedSplit
 
 
-class BlockShuffleSplit(object):
+def permute_label_mapping(labs):
+    """Permute an array of labels, leaving relative positioning intact.
 
-    """
-    Iterate over blocks of indices.
-
-    Cross-validation iterator to shuffle indices based off of labels.
-    These labels can either be given explicitly, or generated in blocks
-    of length n_in_block. The train/test split is based off of the
-    number of block labels, so that no indices from one label will ever
-    make into both the train and test splits.
+    This will find the unique labels in `labs`, shuffle these, and build
+    a mapping of old_label -> new_label.
 
     Parameters
     ----------
-    n : int
-        The length of the indices you wish to generate.
-    n_iter : int
-        The number of iterations for this CV iterator
-    n_in_block : int
-        If automatically generating blocks of labels, how many indices
-        do we want per block.
-    block_indices : array, same length as n
-        Explicitly define block labels. This is useful if you wish to
-        group data points into, for example, trials or trial types.
-        Nullifies
-    test_size : float (between 0 and 1)
-        The proportion of block labels in the test set of each CV.
-        If block labels do not separate into an integer number in each
-        split, then the number in the test set is rounded down (so the
-        test set may have more labels than you thought.)
-    """
-
-    def __init__(self, block_indices=None, n=None, n_iter=10,
-                 n_in_block=10, test_size=0.1, random_state=None):
-
-        if block_indices is None:
-            if n is None or n_in_block is None:
-                raise AssertionError('Supply block_indices, or n + n_in_block')
-            n_blocks = np.ceil(float(n) / n_in_block)
-            self.block_list = np.arange(n_blocks)
-            self.block_indices = np.hstack([[i]*n_in_block
-                                            for i in self.block_list])[:n]
-        else:
-            print('Using pre-supplied block indices')
-            self.block_indices = block_indices
-            self.block_list = np.unique(block_indices)
-            n = block_indices.shape[0]
-
-        self.n = n
-        self.n_iter = n_iter
-        self.n_in_block = n_in_block
-        self.test_size = test_size
-        self.train_size = 1 - test_size
-        self.random_stats = random_state
-        self.indices = np.arange(n)
-        self.n_blocks = len(np.unique(self.block_indices))
-        if self.block_indices.shape[0] != self.n:
-            raise AssertionError('Unequal n and block_indices')
-        print('Using {0} blocks'.format(self.n_blocks))
-
-    def __len__(self):
-        """Return the number of iterations to use."""
-        return self.n_iter
-
-    def __iter__(self):
-        """Iterate over blocks of indices."""
-        for _ in range(self.n_iter):
-            ixs = shuffle_block(self.indices, n=self.n_in_block,
-                                block_indices=self.block_indices)
-            cut = int(np.floor(len(self.block_list) * self.train_size))
-            train_blocks = self.block_list[:cut]
-            test_blocks = self.block_list[cut:]
-            train_i = ixs[np.array([i in train_blocks
-                                    for i in self.block_indices])]
-            test_i = ixs[np.array([i in test_blocks
-                                   for i in self.block_indices])]
-            yield train_i, test_i
-
-
-def shuffle_block(arr, n=10, block_indices=None):
-    '''Shuffles the values in a in a blockwise fashion. This lets you shuffle
-    the values, but keeping nearby ones together.
-
-    Parameters
-    ----------
-    arr : np.array
-        The array you wish to shuffle
-
-    Need one of the next two
-    n : int
-        The size of the blocks that you're shuffling
-    OR
-
-    block_indices : list
-        A list of length arr.shape[0] that specificies block identity for
-        each timepoint
-
+    labs : array-like
+        The labels to be permuted. Unique labels will be randomly swapped
+        with one another.
 
     Returns
     -------
-    a_shuf : array
-        The shuffled array.
-    '''
-    if block_indices is None:
-        blks = np.ceil(float(arr.shape[0]) / n)
-        blklst = np.arange(blks)
-        block_indices = np.hstack([[i]*n for i in blklst])[:arr.shape[0]]
-    else:
-        blklst = np.unique(block_indices)
-    np.random.shuffle(blklst)
-    a_shuf = np.hstack([arr[block_indices==i] for i in blklst])
-    return a_shuf
+    new_labs : array-like
+        a version of labs where each number is randomly flipped with another
+        number, though the overall structure of the labels is the same. E.g.,
+        all 1s may now be 4s.
+    """
+    unique_labels = np.unique(labs)
+    permuted_labels = np.random.permutation(unique_labels)
+    random_mapping = {lab: newlab for lab, newlab
+                      in zip(unique_labels, permuted_labels)}
+    new_labs = np.array([random_mapping[ilab] for ilab in labs])
+    return new_labs
+
+
+def create_cv_from_trials(tnums, test_perc=.1, n_iter=5):
+    """Create a cross validation object using trial numbers.
+
+    This is similar to LeavePLabelOut, but it defines a stopping point and
+    shuffles unique labels before doing the splits. This lets you keep
+    datapoints together in train/test splits.
+
+    Parameters
+    ----------
+    tnums : array, dtype int, shape(n_test)
+        The labels to use for permutation.
+    """
+    tnums = tnums.squeeze()
+    unique_labels = np.unique(tnums).squeeze()
+    n_test = np.floor(unique_labels.shape[0] * test_perc)
+    test_ixs = np.random.permutation(unique_labels)[:n_test * n_iter]
+    test_ixs = test_ixs.reshape([n_iter, n_test])
+    test_fold = np.zeros_like(tnums)
+    for i, ifold in enumerate(test_ixs):
+        for fld in ifold:
+            test_fold[tnums == fld] = i
+    cv = PredefinedSplit(test_fold)
+    return cv
