0eb78b19408cebf823ed11c9bfe95e22d7775bfc
Chris Holdgraf <choldgraf@berkeley.edu>
2016-02-02 19:03:13 -0800

    updating strf code to use the EncodingModel class

--- strf.py
+++ strf.py
@@ -1,17 +1,190 @@
 from __future__ import division
 import numpy as np
-from scipy.linalg import svd
 from sklearn.preprocessing import scale
 from sklearn.cross_validation import KFold, LabelShuffleSplit, LeavePLabelOut
 from sklearn.linear_model import Ridge
 from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import Pipeline
 from mne.utils import _time_mask
 from .utils import embed
 from copy import deepcopy
 
 __all__ = ['svd_clean',
            'mps',
-           'fit_strf']
+           'EncodingModel',
+           'delay_timeseries']
+
+
+class EncodingModel(object):
+    def __init__(self, delays=None, est=None, scorer=None):
+        """Fit a STRF model.
+
+        This implementation uses Ridge regression and scikit-learn. It creates time
+        lags for the input matrix, then does cross validation to fit a STRF model.
+
+        Parameters
+        ----------
+        delays : array, shape (n_delays,)
+            The delays to include when creating time lags. The input array X will
+            end up having shape (n_feats * n_delays, n_times)
+        est : list (instance of sklearn, dict of params)
+            A list specifying the model and parameters to use. First item must be a
+            sklearn regression-style estimator. Second item is a
+            dictionary of kwargs to pass in the construction of that estimator. If
+            any values in kwargs is len > 1, then it is assumed that an inner CV
+            loop is required to select the best value using GridSearchCV.
+        scorer : function | None
+            The scorer to use when evaluating on the held-out test set.
+            It must accept two 1-d arrays as inputs, and output a scalar value.
+            If None, it will be correlation.
+
+        Outputs
+        -------
+        ests : list of sklearn estimators, length (len(cv_outer),)
+            The estimator fit on each cv_outer loop. If len(hyperparameters) > 1,
+            then this will be the chosen model using GridSearch on each loop.
+        scores: array, shape (len(cv_outer),)
+            The scores on the held out test set on each loop of cv_outer
+        X_names : array of strings, shape (n_feats * n_delays)
+            A list of names for each coefficient in the model. It is of structure
+            'name_timedelay'.
+        """
+        self.delays = np.array([0]) if delays is None else delays
+        self.n_delays = len(self.delays)
+        self.est = Ridge() if est is None else est
+        self.scorer = mean_squared_error if scorer is None else scorer
+
+    def fit(self, X, y, sfreq, times=None, tmin=None, tmax=None, cv=None,
+            cv_params=None, feat_names=None):
+        """Fit the model.
+
+        Parameters
+        ----------
+        X : array, shape (n_epochs, n_feats, n_times)
+            The input data for the regression
+        y : array, shape (n_epochs, n_times,)
+            The output data for the regression
+        sfreq : float
+            The sampling frequency for the time dimension
+        times : array, shape (n_times,)
+            The times corresponding to the final axis of x/y. Is used to
+            specify subsets of time per trial (using tmin/tmax)
+        tmin : float | array, shape (n_epochs,)
+            The beginning time for each epoch. Optionally a different time
+            for each epoch may be provided.
+        tmax : float | array, shape (n_epochs,)
+            The end time for each epoch. Optionally a different time for each
+            epoch may be provided.
+        cv : int | instance of (KFold, LabelShuffleSplit)
+            The cross validation object to use for the outer loop
+        feat_names : list of strings/ints/floats, shape (n_feats,) : None
+            A list of values corresponding to input features. Useful for
+            keeping track of the coefficients in the model after time lagging.
+        """
+        if feat_names is not None:
+            if len(feat_names) != X.shape[1]:
+                raise ValueError(
+                    'feat_names and X.shape[0] must be the same size')
+        if times is None:
+            times = np.arange(X.shape[-1]) / float(sfreq)
+        self.tmin = times[0] if tmin is None else tmin
+        self.tmax = times[-1] if tmax is None else tmax
+        self.times = times
+        self.sfreq = sfreq
+
+        # Delay X
+        X, y, labels, names = _build_design_matrix(X, y, sfreq, self.times,
+                                                   self.delays, self.tmin,
+                                                   self.tmax, feat_names)
+        self.feat_names = np.array(names)
+        cv = _check_cv(X, labels, cv, cv_params)
+
+        # Define names for input variabels to keep track of time delays
+        X_names = [(feat, delay)
+                   for delay in self.delays for feat in self.feat_names]
+        self.coef_names = X_names
+
+        # Build model instance
+        if not isinstance(self.est, Pipeline):
+            self.est = Pipeline([('est', self.est)])
+
+        # Create model metadata that we'll add to the obj later
+        mod = self.est.steps[-1][-1]
+        model_data = dict(coefs_all_=[], scores_=[])
+        if isinstance(mod, GridSearchCV):
+            model_data.update(dict(best_estimators_=[], best_params_=[]))
+
+        # Fit the model and collect model results
+        for i, (tr, tt) in enumerate(cv):
+            X_tr = X[:, tr].T
+            X_tt = X[:, tt].T
+            y_tr = y[tr]
+            y_tt = y[tt]
+            lab_tr = labels[tr]
+            lab_tt = labels[tt]
+            self.est.fit(X_tr, y_tr)
+
+            if isinstance(mod, GridSearchCV):
+                # If it's a GridSearch, then add a "best_params" object
+                # Assume hyperparameter search
+                if mod.refit:
+                    model_data['best_estimators_'].append(mod.best_estimator_)
+                    model_data['coefs_all_'].append(mod.best_estimator_.coef_)
+                model_data['best_params_'].append(mod.best_params_)
+            else:
+                model_data['coefs_all_'].append(mod.coef_)
+
+            # Fit model + make predictions
+            scr = self.scorer(self.est.predict(X_tt), y_tt)
+            model_data['scores_'].append(scr)
+
+        for key, val in model_data.iteritems():
+            setattr(self, key, np.array(val))
+        self.coefs_ = np.mean(self.coefs_all_, axis=0)
+        self.cv = cv
+
+    def predict(self, X):
+        X_lag = delay_timeseries(X, self.sfreq, self.delays)
+
+        Xt = self.est._pre_transform(X_lag.T)[0]
+        return np.dot(Xt, self.coefs_)
+
+    def coefs_as_series(self, agg=None):
+        ix = pd.MultiIndex.from_tuples(self.coef_names, names=['feat', 'lag'])
+        if agg is None:
+            sr = []
+            for icv, icoef in enumerate(self.coefs_all_):
+                isr = pd.DataFrame(icoef[:, np.newaxis], index=ix)
+                isr['cv'] = icv
+                isr = isr.set_index('cv', append=True).squeeze()
+                sr.append(isr)
+            sr = pd.concat(sr, axis=0)
+        else:
+            coefs = agg(self.coefs_all_, axis=0)
+            sr = pd.Series(coefs, index=ix)
+        return sr
+
+    def plot_coefficients(self, agg=None, ax=None, cmap=None,
+                          interpolation='nearest', aspect='auto', **kwargs):
+        from matplotlib import pyplot as plt
+        cmap = plt.cm.RdBu_r if cmap is None else cmap
+        agg = np.mean if agg is None else agg
+        if ax is None:
+            f, ax = plt.subplots()
+        df = self.coefs_as_series(agg=agg).unstack('lag')
+        im = ax.imshow(df.values, cmap=cmap, interpolation=interpolation,
+                       aspect=aspect, **kwargs)
+
+        for lab in ax.get_xticklabels():
+            lab.set_text(df.columns[int(lab.get_position()[0])])
+
+        for lab in ax.get_yticklabels():
+            lab.set_text(df.index[int(lab.get_position()[1])])
+
+        ax.set_xlabel('Time delays (s)')
+        ax.set_ylabel('Features')
+        return ax
 
 
 def delay_timeseries(ts, sfreq, delays):
@@ -24,7 +197,10 @@ def delay_timeseries(ts, sfreq, delays):
     sfreq: int
         The sampling frequency of the series
     delays: list of floats
-        The time (in seconds) of each delay
+        The time (in seconds) of each delay. Positive means
+        timepoints in the past, negative means timepoints in
+        the future.
+
     Returns
     -------
     delayed: array, shape(n_feats*n_delays, n_times)
@@ -43,31 +219,6 @@ def delay_timeseries(ts, sfreq, delays):
     return delayed
 
 
-def _cluster_mask(msk):
-    clust_id = 1
-    mask_on = False
-    ixs_clusters = np.zeros_like(msk, dtype=int)
-    for i, ix in enumerate(msk):
-        if ix:
-            if not mask_on:
-                mask_on = True
-            ixs_clusters[i] = clust_id
-        elif not ix:
-            if mask_on:
-                mask_on = False
-                clust_id += 1
-    return ixs_clusters
-
-
-def _times_to_mask(times, sfreq, newlen=None):
-    time_ixs = (times * sfreq).astype(int)
-    masklen = np.max(time_ixs) if newlen is None else newlen
-    mask_time = np.zeros(masklen, dtype=bool)
-    for istt, istp in time_ixs:
-        mask_time[istt:istp] = True
-    return mask_time
-
-
 def _scorer_corr(x, y):
     return np.corrcoef(x, y)[1, 0]
 
@@ -80,172 +231,69 @@ def _check_time(X, time):
     return time
 
 
-def _build_design_matrix(X, y, sfreq, times, delays, tmin, tmax):
-    if X.shape[-1] != y.shape[-1]:
-        raise ValueError('X and y have different time dimension')
+def _check_inputs(X, y, times, delays, tmin, tmax):
+    # Add an epochs dimension
+    if X.ndim == 2:
+        X = X[np.newaxis, ...]
+    if y.ndim == 1:
+        y = y[np.newaxis, ...]
+
+    if not X.shape[-1] == y.shape[-1] == times.shape[-1]:
+        raise ValueError('X, y, or times have different time dimension')
     if X.shape[0] != y.shape[0]:
         raise ValueError('X and y have different number of epochs')
-    if tmin + np.min(delays) < np.min(times):
+    if any([tmin + np.max(delays) < np.min(times),
+            tmax + np.min(delays) > np.max(times)]):
         raise ValueError('Data will be cut off w delays, use longer epochs')
     tmin = _check_time(X, tmin)
     tmax = _check_time(X, tmax)
+    return X, y, tmin, tmax
+
+
+def _build_design_matrix(X, y, sfreq, times, delays, tmin, tmax, names):
+    X, y, tmin, tmax = _check_inputs(X, y, times, delays, tmin, tmax)
+    if names is None:
+        names = [str(i) for i in range(X.shape[1])]
 
     # Iterate through epochs with custom tmin/tmax if necessary
     X_out, y_out, lab_out = [[] for _ in range(3)]
-    for i, (epX, epy, tmin, tmax) in enumerate(zip(X, y, tmin, tmax)):
-        msk_time = _time_mask(times, tmin, tmax)
-
+    for i, (epX, epy, itmin, itmax) in enumerate(zip(X, y, tmin, tmax)):
+        # Create delays
         epX_del = delay_timeseries(epX, sfreq, delays)
+
+        # pull times of interest
+        msk_time = _time_mask(times, itmin, itmax)
         epX_out = epX_del[:, msk_time]
         epy_out = epy[msk_time]
+
+        # Unique labels for this epoch
         ep_lab = np.repeat(i + 1, epy_out.shape[-1])
 
         X_out.append(epX_out)
         y_out.append(epy_out)
         lab_out.append(ep_lab)
-    return np.hstack(X_out), np.hstack(y_out), np.hstack(lab_out)
+    return np.hstack(X_out), np.hstack(y_out), np.hstack(lab_out), names
 
 
-def _check_cv(X, labels, cv):
+def _check_cv(X, labels, cv, cv_params):
     cv = 5 if cv is None else cv
+    cv_params = dict() if cv_params is None else cv_params
     if isinstance(cv, float):
         raise ValueError('cv must be an int or instance of sklearn cv')
-    if isinstance(cv, int):
-        if len(np.unique(labels)) == 1:
-            # Assume single continuous data, do KFold
-            cv = KFold(labels.shape[-1], cv)
-        else:
-            # Assume trials structure, do LabelShufleSplit
-            cv = LabelShuffleSplit(labels, n_iter=cv, test_size=.2)
+
+    if len(np.unique(labels)) == 1:
+        # Assume single continuous data, cv must take a single number
+        if isinstance(cv, int):
+            cv_params = dict(n_folds=cv)
+            cv = KFold
+        cv = cv(labels.shape[-1], **cv_params)
+    else:
+        # Assume trials structure, cv must take a set of labels for trials
+        if isinstance(cv, int):
+            cv_params = dict(n_iter=cv)
+            cv = LabelShuffleSplit
+        cv = cv(labels, **cv_params)
     return cv
-
-
-def fit_strf(X, y, sfreq, times, delays, tmin=None, tmax=None, est=None,
-             cv_outer=None, cv_inner=None, scorer_outer=None,
-             X_names=None, scale_data=True):
-    """Fit a STRF model.
-
-    This implementation uses Ridge regression and scikit-learn. It creates time
-    lags for the input matrix, then does cross validation to fit a STRF model.
-
-    Parameters
-    ----------
-    X : array, shape (n_epochs, n_feats, n_times)
-        The input data for the regression
-    y : array, shape (n_times,)
-        The output data for the regression
-    sfreq : float
-        The sampling frequency for the time dimension
-    delays : array, shape (n_delays,)
-        The delays to include when creating time lags. The input array X will
-        end up having shape (n_feats * n_delays, n_times)
-    tmin : float | array, shape (n_epochs,)
-        The beginning time for each epoch. Optionally a different time for each
-        epoch may be provided.
-    tmax : float | array, shape (n_epochs,)
-        The end time for each epoch. Optionally a different time for each
-        epoch may be provided.
-    est : list (instance of sklearn, dict of params)
-        A list specifying the model and parameters to use. First item must be a
-        sklearn regression-style estimator. Second item is a
-        dictionary of kwargs to pass in the construction of that estimator. If
-        any values in kwargs is len > 1, then it is assumed that an inner CV
-        loop is required to select the best value using GridSearchCV.
-    cv_outer : int | instance of (KFold, LabelShuffleSplit)
-        The cross validation object to use for the outer loop
-    cv_inner : int | instance of same type as cv_outer
-        The cross validation object to use for the inner loop,
-        if hyperparameters are to be chosen computationally.
-    scorer_outer : function | None
-        The scorer to use when evaluating on the held-out test set.
-        It must accept two 1-d arrays as inputs, and output a scalar value.
-        If None, it will be correlation.
-    X_names : list of strings/ints/floats, shape (n_feats,) : None
-        A list of values corresponding to input features. Useful for keeping
-        track of the coefficients in the model after time lagging.
-    scale_data : bool
-        Whether or not to scale the data to 0 mean and unit var before fit.
-
-    Outputs
-    -------
-    ests : list of sklearn estimators, length (len(cv_outer),)
-        The estimator fit on each cv_outer loop. If len(hyperparameters) > 1,
-        then this will be the chosen model using GridSearch on each loop.
-    scores: array, shape (len(cv_outer),)
-        The scores on the held out test set on each loop of cv_outer
-    X_names : array of strings, shape (n_feats * n_delays)
-        A list of names for each coefficient in the model. It is of structure
-        'name_timedelay'.
-    """
-    # Checks
-    if len(X_names) != X.shape[1]:
-        raise ValueError('X_names and X.shape[0] must be the same size')
-    scorer_outer = _scorer_corr if scorer_outer is None else scorer_outer
-
-    # Delay X
-    X, y, labels = _build_design_matrix(X, y, sfreq, times, delays, tmin, tmax)
-    cv_outer = _check_cv(X, labels, cv_outer)
-
-    if scale_data:
-        # Scale features
-        print('Scaling features...')
-        X = scale(X, axis=-1)
-        y = scale(y, axis=-1)
-
-    X_names = [str(i) for i in range(len(X))]if X_names is None else X_names
-    X_names = ['{0}_{1}'.format(feat, delay)
-               for delay in delays for feat in X_names]
-
-    # Build model instance
-    est = [Ridge, dict(fit_intercept=False, alpha=1)] if est is None else est
-    mod, kws = est
-    hp_gridsearch = [isinstance(i, (list, tuple)) for i in kws.values()]
-
-    # Fit the models
-    ests, scores = [[] for _ in range(2)]
-    for i, (tr, tt) in enumerate(cv_outer):
-        print('\nCV: {0}'.format(i))
-        X_tr = X[:, tr].T
-        X_tt = X[:, tt].T
-        y_tr = y[tr]
-        y_tt = y[tt]
-        lab_tr = labels[tr]
-        lab_tt = labels[tt]
-
-        # Build the estimator
-        if any(hp_gridsearch):
-            # Have hyperparameters to loop through
-            if not isinstance(cv_outer, type(cv_inner)):
-                raise ValueError('cv objects must be of same type')
-            if isinstance(cv_outer, LabelShuffleSplit):
-                cv_inner = LabelShuffleSplit(lab_tr, n_iter=5, test_size=.2)
-            else:
-                # Assume kfold
-                cv_inner = KFold(len(y_tr), n_folds)
-            kws_fit = dict(estimator=deepcopy(mod),
-                           param_grid=kws, cv=cv_inner)
-            mod_fit = GridSearchCV
-
-        else:
-            mod_fit = deepcopy(mod)
-            kws_fit = deepcopy(kws)
-        mod_fit = mod_fit(**kws_fit)
-
-        # Fit model + make predictions
-        mod_fit.fit(X_tr, y_tr)
-        pred = mod_fit.predict(X_tt)
-        scr = scorer_outer(pred, y_tt)
-
-        if any(hp_gridsearch):
-            print('Alpha scores\n{}'.format(
-                '\n'.join([str(s) for s in mod_fit.grid_scores_])))
-            mod_fit = mod_fit.best_estimator_
-        else:
-            print('Score: {0}'.format(scr))
-
-        scores.append(scr)
-        ests.append(mod_fit)
-    return ests, np.array(scores), np.array(X_names)
 
 
 def svd_clean(arr, svd_num=[0], kind='ix'):
@@ -272,6 +320,7 @@ def svd_clean(arr, svd_num=[0], kind='ix'):
     clean_all : np.array
         The cleaned input array
     '''
+    from scipy.linalg import svd
     U, s, Vh = svd(arr, full_matrices=False)
     if kind == 'perc':
         s_scaled = s / np.sum(s)
