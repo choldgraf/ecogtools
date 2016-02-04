94cc1b9195c2d318bd31de33859f296226f93bd0
Chris Holdgraf <choldgraf@berkeley.edu>
2016-02-04 13:53:24 -0800

    making sfreqs optional and updating docstrings

--- mneutils.py
+++ mneutils.py
@@ -34,7 +34,28 @@ def create_random_raw(nchan, ntime, sfreq, ch_types='eeg'):
 
 class Events(object):
     """Container for events metadata"""
-    def __init__(self, time_start, sfreq, time_stop=None, meta=None):
+    def __init__(self, time_start, sfreq=None, time_stop=None, meta=None):
+        """Create an Events object to easily translate to MNE events array.
+
+        Parameters
+        ----------
+        time_start : array, shape (n_events,)
+            An array of start times (in seconds) for the events of interest.
+        sfreq : None | float
+            The default sampling frequency for these events.
+        time_stop : None | array, shape (n_events,)
+            Optionally, the stop time for each event. Must be the same length
+            as time_start.
+        meta : None | pandas DataFrame, shape (n_events, n_meta_columns)
+            An optional metadata container for events. This is expected to have
+            event type strings as values, with each row corresponding to an
+            event and each column corresponding to a class of event types.
+
+        Attributes
+        ----------
+        length : array, shape (n_events,)
+            If time_stop is given, it is the length of each event.
+        """
         self.time_start = np.array(time_start)
         if time_stop is not None:
             time_stop = np.array(time_stop)
@@ -53,8 +74,32 @@ class Events(object):
         self.sfreq = sfreq
         self.ids = np.arange(len(time_start))
 
-    def to_mne(self, meta_columns=None):
-        times = self.time_start * self.sfreq
+    def to_mne(self, sfreq=None, meta_columns=None):
+        """Convert the events to MNE shape.
+
+        Parameters
+        ----------
+        sfreq : None | float
+            The sampling frequency used to calculate indices
+        meta_columns : None | list of strings
+            The columns to use as trial types in the output array. If
+            a list is given, each string in `meta_columns` must be in
+            self.meta.columns. For each epoch (row) of self.meta, unique
+            combinations of epoch types are tallied, and the corresponding
+            strings/event_id mappings are given in einf. If None, then
+            a unique string for each epoch will be generated.
+
+        Returns
+        -------
+        events : array, shape (n_epochs, 3)
+            An MNE events array.
+        einf : dict
+            The dictionary mapping of event_string: event_id for epoch types.
+        """
+        sfreq = self.sfreq if sfreq is None else sfreq
+        if sfreq is None:
+            raise ValueError('A sampling frequency must be given')
+        times = self.time_start * sfreq
         zeros = np.zeros_like(times)
 
         if meta_columns is not None:
