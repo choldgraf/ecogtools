97b438b46b709c9c2759b7b4b41ebb1ab643d06f
Chris Holdgraf <choldgraf@berkeley.edu>
2016-12-23 13:41:11 -0600

    cleaning up tfr and viz modules for unused objects

--- ecogtools/viz3d.py
+++ ecogtools/viz3d.py
@@ -6,8 +6,6 @@ from plotly import graph_objs as go
 from plotly import offline as offl
 from copy import deepcopy
 from .viz import array_to_plotly
-from mayavi.core.ui.mayavi_scene import MayaviScene
-from mayavi import mlab
 from surfer import Brain
 
 
@@ -255,71 +253,3 @@ def set_lights(fig, elevation=5, azimuth=60, intensity=1.,
 
     if n_lights is not None:
         fig.scene.light_manager.number_of_lights = n_lights
-
-
-# --- Mlab Functions ---
-def _get_world_to_view_matrix(mlab_scene):
-    """returns the 4x4 matrix that is a concatenation of the
-    modelview transform and perspective transform. Takes as
-    input an mlab scene object."""
-
-    if not isinstance(mlab_scene, MayaviScene):
-        raise TypeError('argument must be an instance of MayaviScene')
-
-    cam = mlab_scene.camera
-
-    # The VTK method needs the aspect ratio and near and far
-    # clipping planes in order to return the proper transform.
-    # So we query the current scene object to get the parameters we need.
-    scene_size = tuple(mlab_scene.get_size())
-    clip_range = cam.clipping_range
-    aspect_ratio = float(scene_size[0])/float(scene_size[1])
-
-    # this actually just gets a vtk matrix object, we can't really
-    # do anything with it yet
-    vtk_comb_trans_mat = cam.get_composite_perspective_transform_matrix(
-        aspect_ratio, clip_range[0], clip_range[1])
-
-    # get the vtk mat as a numpy array
-    np_comb_trans_mat = vtk_comb_trans_mat.to_array()
-
-    return np_comb_trans_mat
-
-
-def _get_view_to_display_matrix(mlab_scene):
-    """ Return a 4x4 matrix to convert view coordinates to display coordinates.
-
-    It's assumed that the view should take up the entire window and that the
-    origin of the window is in the upper left corner.
-    """
-
-    if not (isinstance(mlab_scene, MayaviScene)):
-        raise TypeError('argument must be an instance of MayaviScene')
-
-    # this gets the client size of the window
-    x, y = tuple(mlab_scene.get_size())
-
-    # normalized view coordinates have the origin in the middle of the space
-    # so we need to scale by width and height of the display window and shift
-    # by half width and half height. The matrix accomplishes that.
-    view_to_disp_mat = np.array([[x/2.0,      0.,   0.,   x/2.0],
-                                 [   0.,  -y/2.0,   0.,   y/2.0],
-                                 [   0.,      0.,   1.,      0.],
-                                 [   0.,      0.,   0.,      1.]])
-
-    return view_to_disp_mat
-
-
-def _apply_transform_to_points(points, trans_mat):
-    """Applies a 4x4 transformation matrix to a matrix of homogeneous points.
-
-    The array of points should have shape Nx4.
-    """
-
-    if not trans_mat.shape == (4, 4):
-        raise ValueError('transform matrix must be 4x4')
-
-    if not points.shape[1] == 4:
-        raise ValueError('point array must have shape Nx4')
-
-    return np.dot(trans_mat, points.T).T
